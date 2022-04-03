cffb96ca58283d9a76e3eb8b0e009e46503e2aeb
anagpure28 <92313981+anagpure28@users.noreply.github.com>
2022-04-03 15:34:55 +0530

    updated

--- 3_Implementation/src/movies.c
+++ 3_Implementation/src/movies.c
@@ -5,9 +5,9 @@ int movieselect(void)
 	int i;
 	printf("\t\t\tMovie Name\n");
 	printf("\t\t\t-------------------------------------\n");
-	printf("\t\t\tpress 1 for Harry Potter\n\n");
-	printf("\t\t\tpress 2 for The Invisible Man\n\n");
-	printf("\t\t\tpress 3 for Tenet\n");
+	printf("\t\t\tpress 1 for War\n\n");
+	printf("\t\t\tpress 2 for RRRn\n\n");
+	printf("\t\t\tpress 3 for Bahubali\n");
 	scanf("%d",&i);
 	return i;
 }
