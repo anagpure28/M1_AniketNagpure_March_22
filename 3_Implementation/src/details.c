d66e632bb9cec4497d5f8321a3af07a5935b5961
anagpure28 <92313981+anagpure28@users.noreply.github.com>
2022-04-03 15:30:49 +0530

    updated

--- 3_Implementation/src/details.c
+++ 3_Implementation/src/details.c
@@ -8,17 +8,17 @@ void details(void)
 	int i;
 	int count =0;
 	char pass[10],pak[10]="admin";
-	printf("Enter the password to see details: ");
+	printf("Enter the registered password to see your details: ");
 	scanf("%s",&pass);
 	if (strcmp(pass,pak)==0)
 	{
 		for (i=0;i<count;i++)
 		{
-			printf("seat no: %d is booked by %s booking id is %d\n",person[i].seatnum,person[i].name,person[i].id);
+			printf("seat no: %d is booked by %s booking id of the person is %d\n",person[i].seatnum,person[i].name,person[i].id);
 		}
 	}
 	else
-		printf("Entered password is wrong \n");
+		printf("The Entered password is Wrong \n");
 		system("PAUSE");
 		system("CLS");
 }
